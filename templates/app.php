45cd8aac323cc8115c642bd93ac5928b776e45c4
Rustam Bekmansurov <rustam1204@icloud.com>
2026-06-11 22:58:29 +0500

    upd

--- templates/app.php
+++ templates/app.php
@@ -411,8 +411,7 @@ $basePath = $basePath ?? '';
     </main>
   </div>
 
-  <script src="https://code.jquery.com/jquery-3.7.1.min.js"
-    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+  <script src="<?= htmlspecialchars($basePath) ?>/assets/js/jQuery/jquery-3.7.1.min.js"></script>
   <script src="<?= htmlspecialchars($basePath) ?>/assets/js/app.js"></script>
 </body>
 
